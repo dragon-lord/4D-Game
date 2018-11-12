92ab5b738079773e3bda5d716d7ea96ef636b877
dragon-lord <dragon1kid@gmail.com>
2018-11-12 16:49:56 -0600

    Added 3D Texture Mapping

--- texture-00/main.c
+++ texture-00/main.c
@@ -6,12 +6,12 @@
 #include "render.h"
 #include "inputhandeler.h"
 
-struct Texture test;
-struct Vec2 poly[4]={{0,-0.5},{1,-1},{1,1},{0,0.5}};
-struct Vec2 src[4]={{-1,-1},{1,-1},{1,1},{-1,1}};
+struct Texture3 test;
+struct Vec2 poly[4]={{-1,-1},{1,-1},{1,1},{-1,1}};
+struct Vec3 src[4]={{-1,-1,1},{1,-1,1},{1,1,1},{-1,1,1}};
 
 void game(float fps){
-  Render_mappedTexture(test,poly,src,4);
+  Render_mappedTexture3(test,poly,src,4);
 }
 
 int main(int argc,char **argv){
@@ -21,15 +21,33 @@ int main(int argc,char **argv){
     return -1;
   }
 
-  test=Load_image("test_image-01.png");
+  test.w=200;
+  test.h=200;
+  test.d=200;
+  test.pixels=malloc((test.d+1)*(test.w+1)*(test.h+1)*sizeof(Uint32));
+  memset(test.pixels,9,(test.d+1)*(test.w+1)*(test.h+1)*sizeof(Uint32));
+  for(int i=0;i<=test.w;i++){
+    for(int j=0;j<=test.h;j++){
+      for(int k=0;k<=test.d;k++){
+        int x=i-test.w/2;
+        int y=j-test.h/2;
+        int z=k-test.d/2;
+        test.pixels[test.w*test.h*k+test.w*j+i]=(int)creal(csqrt(20000-(x*x+y*y+z*z)));
+      }
+    }
+  }
+
+  /*test=Load_image("test_image.jpg");
   if(test.pixels==NULL){
+    free(test.pixels);
     Render_destroy();
     return -1;
-  }
+  }//*/
 
   while(!quit){
     Render_update(game);
     Input_update();
+    //quit=1;
   }
 
   free(test.pixels);
